e69e94554454bf9a69aa1f40388169d53b62765d
lamarmellitus1 <92574617+lamarmellitus1@users.noreply.github.com>
2024-05-21 12:08:00 +0100

    Create script.groovy

---
+++ script.groovy
@@ -0,0 +1,16 @@
+def buildjar(){
+    echo 'building a jar file'
+    sh 'mvn clean package'
+}
+
+def buildImage(){
+   echo 'building a docker image & deploying to dockerhub'
+                      withCredentials([string(credentialsId: 'passwds', variable: 'dockerwd')])
+                      sh 'docker build -t mellitus/java-web-app:latest . '
+                    sh "docker login -u mellitus -p ${dockerwd}"
+                    sh "docker push mellitus/java-web-app:latest"
+}
+
+def deploy(){
+    echo 'deploying'
+}
