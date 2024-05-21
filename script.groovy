285f41dc263f73c1d2b2e37a42ba603ee07e9c44
lamarmellitus1 <92574617+lamarmellitus1@users.noreply.github.com>
2024-05-21 14:40:28 +0100

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -5,10 +5,12 @@ def buildjar(){
 
 def buildImage(){
    echo 'building a docker image & deploying to dockerhub'
-                      withCredentials([string(credentialsId: 'passwds', variable: 'dockerwd')])
-                      sh 'docker build -t mellitus/java-web-app:latest . '
+                    withCredentials([string(credentialsId: 'passwds', variable: 'dockerwd')]){
+                    sh 'docker build -t mellitus/java-web-app:latest . '
                     sh "docker login -u mellitus -p ${dockerwd}"
                     sh "docker push mellitus/java-web-app:latest"
+                      }
+                     
 }
 
 def deploy(){
